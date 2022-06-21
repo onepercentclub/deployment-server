167c2a0ed35d71ad9ac237ccdb8239bbf895c1d1
Ernst Odolphi <ernst@goodup.com>
2022-06-21 16:27:58 +0200

    Small fix

--- deployments.py
+++ deployments.py
@@ -33,24 +33,8 @@ github.headers.update({
 })
 
 
-def make_celery(app):
-    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
-                    broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-
-    TaskBase = celery.Task
-
-    class ContextTask(TaskBase):
-        abstract = True
-
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-
-
-celery = make_celery(app)
+client = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+client.conf.update(app.config)
 
 
 @app.route('/webhook/', methods=['GET', 'POST'])
